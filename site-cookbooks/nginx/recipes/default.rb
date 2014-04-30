c1a6d7197e363f095f7e3d0f180e77e55bee7880
kwmur <alarm.clock.website@gmail.com>
2014-04-30 17:20:10 +0900

    chef-repoの中身を上のディレクトリへ移動

--- site-cookbooks/nginx/recipes/default.rb
+++ site-cookbooks/nginx/recipes/default.rb
@@ -16,3 +16,12 @@ service "nginx" do
   action [:enable, :start]
 end
 
+template "nginx.conf" do
+  path "/etc/nginx/nginx.conf"
+  source "nginx.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :reload , "service[nginx]"
+end
+
